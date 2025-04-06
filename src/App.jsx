5e9111bd26bac36e7b4b24754daf567e01372ee9
Michael Wayne Davis <info@yeet.services>
2025-04-05 22:07:42 -0400

    fix Buffer is not defined error for Solana transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+
 import React, { useState, useEffect } from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { useWallet } from '@solana/wallet-adapter-react';
